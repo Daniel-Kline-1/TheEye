645a7c3ec839bfd44bc3b10133c6ce65e737b717
Daniel Kline <danielkline@Daniels-Mac-mini.local>
2021-10-23 17:00:47 -0400

    added get request for sessions

--- eye/urls.py
+++ eye/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
 
-from .views import EventView
+from .views import EventView,SessionView
 
 
 urlpatterns = [
-    path('event/', EventView.as_view(), name='Events')
+    path('event/', EventView.as_view(), name='Events'),
+    path('session/', SessionView.as_view(), name='session' ),
+    
 ]
