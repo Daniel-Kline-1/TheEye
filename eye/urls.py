c075f3869f7c456b8c878f6289fb062fcfc581a1
Daniel Kline <danielkline@Daniels-Mac-mini.local>
2021-10-23 16:25:03 -0400

    added migrations and simple post function

---
+++ eye/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+
+from .views import EventView
+
+
+urlpatterns = [
+    path('event/', EventView.as_view(), name='Events')
+]
--- eye/views.py
+++ eye/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
 
 # Create your views here.
+
+class EventView(APIView):
+    def post(self,request,*args,**kwargs):
+        print('x')
+        return Response(data={'it worked'})
